ec500177fbb53632ae8233f739715f8f302941fc
puterakahfi <puterakahfi@gmail.com>
2020-01-25 11:57:50 +0700

    ignore .env and writeable directory

--- app/Views/Stisla/Layout/sidebar.php
+++ app/Views/Stisla/Layout/sidebar.php
@@ -225,7 +225,7 @@
             </div>
           </li>
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-            <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+            <img alt="image" src="<?php echo base_url('stisla/img/avatar/avatar-1.png'); ?>" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
             <div class="dropdown-menu dropdown-menu-right">
               <div class="dropdown-title">Logged in 5 min ago</div>
